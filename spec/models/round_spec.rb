0891ebbcd2b44ee506724572eb5bcf2fdbf9d671
Brett Lischalk <brett@brettlischalk.com>
2012-08-06 15:30:05 -0500

    Added some round specs

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Round do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when initialized" do
+    let(:round) { Round.create(phrase: FactoryGirl.create(:phrase)) }
+    its(:phrase_indicies) { should be_a(Hash) }
+    context "#set_data" do
+      it 'should have an incorrect answer' do
+        round.set_data('a', false)
+        round.incorrect_answers.should be(1)
+      end
+      it 'should not have an incorrect answer' do
+        round.set_data('a', true)
+        round.incorrect_answers.should be(0)
+      end
+    end
+    context "#data" do
+      its(:data) { should == { phrase_indicies: {}, incorrect_answers: 0 } }
+    end
+    it 'updates phrase indicies'
+  end
 end
